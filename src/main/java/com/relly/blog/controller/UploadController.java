d92762c84cf84831eb81848cca87df9986e6fc51
kawinCheng <707471634@qq.com>
2018-11-20 13:51:39 +0800

    文件上传

---
+++ src/main/java/com/relly/blog/controller/UploadController.java
@@ -0,0 +1,78 @@
+package com.relly.blog.controller;
+
+import com.relly.blog.common.exception.ServiceException;
+import com.relly.blog.common.model.JsonResult;
+import com.relly.blog.utils.FileUtil;
+import com.relly.blog.utils.MD5salt;
+import com.relly.blog.vo.UploadFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/upload")
+public class UploadController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
+
+
+    @PostMapping("/singleUpload")
+    public JsonResult upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        if (file.isEmpty()) {
+            throw new ServiceException("上传文件失败,请选择文件");
+        }
+        String fileName = file.getOriginalFilename();
+        //获取后缀
+        int begin=fileName.indexOf(".");
+        int last=fileName.length();
+        String back = fileName.substring(begin,last);
+        //加密文件名称
+        Map<String,String> map = MD5salt.MD5File(fileName);
+        fileName = map.get("md5file")+back;
+        String filePath = request.getSession().getServletContext().getRealPath("uploadFile/");
+        try {
+            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            throw new ServiceException(""+e+"");
+        }
+        Map<String,Object> fileMap = new HashMap<>();
+        fileMap.put("originName",file.getOriginalFilename());
+        fileMap.put("filePath",filePath+fileName);
+        return new JsonResult(fileMap);
+    }
+
+    @PostMapping("/multiUpload")
+    @ResponseBody
+    public JsonResult multiUpload(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        String filePath = request.getSession().getServletContext().getRealPath("uploadFile/");
+        List<UploadFile> fileList = new ArrayList<>();
+        for (int i = 0; i < files.size(); i++) {
+            MultipartFile file = files.get(i);
+            if (file.isEmpty()) {
+                throw new ServiceException("上传文件失败!请选择文件!");
+            }
+            String fileName = file.getOriginalFilename();
+            try {
+                FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+            } catch (Exception e) {
+                throw new ServiceException(""+e+"");
+            }
+            UploadFile uploadFile = UploadFile.builder()
+                    .filePath(filePath+fileName)
+                    .originName(file.getOriginalFilename())
+                    .build();
+            fileList.add(uploadFile);
+        }
+        return new JsonResult(fileList);
+    }
+
+}
--- src/main/java/com/relly/blog/utils/MD5salt.java
+++ src/main/java/com/relly/blog/utils/MD5salt.java
@@ -3,6 +3,7 @@ package com.relly.blog.utils;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +19,16 @@ public class MD5salt {
         map.put("pwd",String.valueOf(result));
         map.put("salt",String.valueOf(salt));
         map.put("verify",String.valueOf(verify));
+        return map;
+    }
+
+    public static Map<String,String>  MD5File(String fileName){
+        String hashAlgorithmName = "MD5";//加密方式
+        ByteSource salt = ByteSource.Util.bytes(fileName);//加盐
+        int hashIterations = 2;//加密2次
+        Object result = new SimpleHash(hashAlgorithmName,fileName,salt,hashIterations);
+        Map<String,String> map = new HashMap<>();
+        map.put("md5file",String.valueOf(result));
         return map;
     }
 }
